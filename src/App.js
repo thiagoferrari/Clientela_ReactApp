cad3610ee9305833428c48847bb97dc7f4db0024
Thiago Ferrari <thiagofcjr@gmail.com>
2021-05-23 21:58:43 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import TopBar from './ui/TopBar';
+import FooterBar from './ui/FooterBar'
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import cyan from '@material-ui/core/colors/cyan';
+import pink from '@material-ui/core/colors/pink';
+import { Box } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ClientesList from './routed/ClientesList';
+import ClientesForm from './routed/ClientesForm';
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: cyan[500],
+        },
+        secondary: {
+            main: pink[500],
+        },
+    },
+})
+
+const useStyles = makeStyles((theme) => ({
+    box: {
+        backgroundColor: theme.palette.background.default,
+        minHeight: '100vh', // 100% da altura visível
+        paddingBottom: '42px' //para que o conteúdo não fique escondido atrás do footer
+    },
+    routed: {
+        padding: '24px',
+        color: theme.palette.text.primary,
+        fonFamily: theme.typography.fontFamily
+    }
+}))
+
+function Main() {
+    const classes = useStyles()
+    return (
+        <Box className={classes.box}>
+            <BrowserRouter>
+                <TopBar />
+                <Box id='routed' className={classes.routed}>
+                    <Switch>
+                        <Route path="/list">
+                            <ClientesList />
+                        </Route>
+                        <Route path="/new">
+                            <clientesForm />
+                        </Route>
+                    </Switch>
+                </Box>
+                <FooterBar />
+            </BrowserRouter>
+        </Box>
+    )
+}
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Main />
+        </ThemeProvider>
+    );
+}
+
+export default App;
---
+++ src/ui/MainMenu.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom'
+
+
+const useStyles = makeStyles((theme) => ({
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    menuLink: {
+        color: theme.palette.text.primary,
+        textDecoration: 'none'
+    }
+}));
+
+export default function MainMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const classes = useStyles()
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                <MenuIcon />
+            </IconButton>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose} >
+                    <Link className={classes.menuLink} to='/list'>listagem de clientes</Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose} >
+                    <Link className={classes.menuLink} to='/new'>cadastrar cliente</Link>
+                </MenuItem>
+            </Menu>
+        </div>
+    );
+}
